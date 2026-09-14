42bbbe67a769cd02f26307955a2168692d1a263f
XYLO Tech Solution <xylo.info1@gmail.com>
2026-09-14 14:35:36 +0600

    Update dbConnect.ts

--- xylo/lib/dbConnect.ts
+++ xylo/lib/dbConnect.ts
@@ -1,29 +1,3 @@
-// lib/dbConnect.ts
-import mongoose from "mongoose";
-
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local",
-  );
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
-
-let cached = (global as any).mongoose || { conn: null, promise: null };
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
-export default dbConnect;
